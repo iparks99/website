355b02401ef2e9afd14d1b08092abee3d3add673
Ian Parks <ian.parks99@gmail.com>
2019-01-20 14:28:33 -0500

    Added basic animation support.

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,14 @@ jQueryScript.setAttribute('src', 'https://code.jquery.com/jquery-3.3.1.js');
 document.head.appendChild(jQueryScript);
 
 $(document).ready(function () {
-	$(".slide").click(function (event) {
-		$(this).fadeOut("slow", function () {});
-	});
+	$("#slideshow > div:gt(0)").hide();
+
+	setInterval(function() {
+		$('#slideshow > div:first')
+			.fadeOut(1000)
+			.next()
+			.fadeIn(1000)
+			.end()
+			.appendTo('#slideshow');
+	}, 3000);
 });
