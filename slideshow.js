74c22fe37a3a2600f0b3b93cdfdb8ccc603e35ba
Ian Parks <ian.parks99@gmail.com>
2019-01-20 19:31:31 -0500

    B1: Made slideshows independent of one another. Now allows for multiple slideshows on a single page.

--- slideshow.js
+++ slideshow.js
@@ -2,15 +2,21 @@ var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src', 'https://code.jquery.com/jquery-3.3.1.js');
 document.head.appendChild(jQueryScript);
 
+// Only executes once the DOM is fully loaded.
 $(document).ready(function () {
-	$("#slideshow > div:gt(0)").hide();
+	// Hides every slideshow's images except for its first image.
+	$("div[id='slideshow']").each(function(index) {
+		$(this).children("div:gt(0)").hide();
+	});
 
 	setInterval(function() {
-		$('#slideshow > div:first')
-			.fadeOut(1000)
-			.next()
-			.fadeIn(1000)
-			.end()
-			.appendTo('#slideshow');
+		$("div[id='slideshow']").each(function(index) {
+			$(this).children("div:first")
+				.fadeOut(1000)
+				.next()
+				.fadeIn(1000)
+				.end()
+				.appendTo($(this));
+		})
 	}, 3000);
 });
